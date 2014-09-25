1f66da4ffb4a413dd9f2a88a91e70d096bb3f74f
Casey Jones <jonescaseyb@gmail.com>
2014-09-25 10:23:31 -0500

    Add some more opcodes

--- chip8.h
+++ chip8.h
@@ -1,14 +1,21 @@
+#define DISP_T 64*32 //Display buffer size
+
+struct stack_t {
+	unsigned short i;
+	struct stack_t *next;
+};
+
 struct chip8 {
 	unsigned short opcode;
 	unsigned char memory[4096];
 	unsigned char V[16];	//registers
 	unsigned short I; 		//index register
 	unsigned short pc; 		//program counter
-	unsigned char gfx[64*32];
+	unsigned char gfx[DISP_T];
 	unsigned char drawFlag;
 	unsigned char delay_timer;
 	unsigned char sound_timer;
-	unsigned short stack[16];
+	struct stack_t stack;
 	unsigned short sp;		//stack pointer
 	unsigned char key[16];	//keypad
 };
--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "chip8.h"
+
+void stack_init(struct stack_t *s)
+{
+	s->next = NULL;
+}
+
+void stack_push(struct stack_t *s, int i)
+{
+	struct stack_t curr;
+	curr.i = i;
+	curr.next = s;
+	s = &curr;
+}
+
+int stack_pop(struct stack_t *s)
+{
+	if(s == NULL)
+		return 0;
+		
+	int i = s->i;
+	s = s->next;
+	return i;
+}
 
 void load_game(struct chip8 *c, char* game)
 {
@@ -61,6 +85,8 @@ void initialize(struct chip8 *c)
 	}
 
 	//reset timers
+	c->delay_timer = 0;
+	c->sound_timer = 0;
 }
 
 void emulate_cycle(struct chip8 *c)
@@ -68,19 +94,89 @@ void emulate_cycle(struct chip8 *c)
 	//fetch 
 	c->opcode = c->memory[c->pc] << 8 | c->memory[c->pc+1];
 	c->pc += 2;
+	int vx = c->opcode & 0x0F00 >> 8;
+	int vy = c->opcode & 0x00F0 >> 4;
 
 	//decode opcode
 	switch(c->opcode & 0xF000)
 	{
-		case 0x1000: //Jumps to address NNN
+		case 0x0000: 
+			switch(c->opcode & 0x000F)
+			{
+				case 0x0000: //0x00E0: clears the screen
+					memset(c->gfx, 0, DISP_T);
+					c->drawFlag = 1;
+					break;
+				case 0x000E: //0x00EE: returns from subroutine
+					c->pc = stack_pop(&c->stack);
+					c->sp -= 1;
+					break;
+				default:
+					printf("Unknown opcode [0x0000]: 0x%X\n", c->opcode);
+			}
+			break;
+		case 0x1000: //1NNN: Jumps to address NNN
 			c->pc = c->opcode & 0x0FFF;
 			break;
-		//execute opcode
+		case 0x2000: //2NNN: Call subroutine at NNN
+			stack_push(&c->stack, c->pc);
+			c->pc = c->opcode & 0x0FFF;
+			break;
+		case 0x3000: //3NNN: skip instruction if V[x]= NN
+			if(c->V[vx] == c->opcode & 0x00FF)
+				c->pc += 2;
+			break;
+		case 0x4000: //4xNN: skip instruction if V[x] != NN
+			if(c->V[vx] != c->opcode & 0x00FF)
+				c->pc += 2;
+			break;
+		case 0x5000: //5xy0: skip instruction if V[x] == V[y]
+			if(c->V[vx] == c->V[vy])
+				c->pc += 2;
+			break;
+		case 0x6000: //6xNN: V[x] = NN
+			c->V[vx] = c->opcode & 0x00FF;
+			break;
+		case 0x7000: //7xNN: V[x] += NN
+			c->V[vx] += c->opcode & 0x00FF;
+			break;
+		case 0x8000:
+			switch(c->opcode & 0x000F)
+			{
+				case 0x0000: //8xy0: V[x] = V[y]
+					c->V[vx] = c->V[vy];
+					break;
+				case 0x0001: //8xy1: V[x] = V[x] OR V[y]
+					c->V[vx] = c->V[vx] | c->V[vy];
+					break;
+				case 0x0002: //8xy2: V[x] = V[x] AND V[y]
+					c->V[vx] = c->V[vx] & c->V[vy];
+					break;
+				case 0x0003: //8xy3: V[x] = V[x] XOR V[y]
+					c->V[vx] = c->V[vx] ^ c->V[vy];
+					break;
+				case 0x0004: //8xy4: V[x] += V[y]
+					if(c->V[vx] + c->V[vy] > 0xFF)
+						c->V[0xF] = 1;
+					else
+						c->V[0xF] = 0;
+					c->V[vx] += c->V[vy];
+					c->V[vx] &= 0xFF;
+					break;
+				case 0x0005: //8xy5: V[x] -= V[y]
+					if(c->V[vx] > c->V[vy])
+						c->V[0xF] = 1;
+					else
+						c->V[0xF] = 0;
+					c->V[vx] -= c->V[vy];
+					c->V[vx] &= 0xFF;
+					break;
+			}
 		case 0xA000: //ANNN sets index register to the address NNN
 			c->I = c->opcode & 0x0FFF;
 			break;
 
-		default: //unknown opcode
+		default:
 			printf("unknown opcode 0x%X\n", c->opcode);
 	}
 
@@ -107,16 +203,16 @@ int main(int argc, char** argv)
 	//setup graphics
 	//setup input
 
-	/*for(;;)
+	for(;;)
 	{
-		emulate_cycle(cpu);
-		if(cpu->drawFlag)
+		emulate_cycle(&cpu);
+		if(cpu.drawFlag)
 		{
 			//draw graphics
 		}
 
 		//set keys
-	}*/
+	}
 
 	return 0;
 }
